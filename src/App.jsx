ff918eb4f57a3906a693c8c7fe8eac3b6db8d15f
rasamkamyar <rasamkamyar@gamil.com>
2024-02-20 13:43:25 +0330

    finish search bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,33 @@ import style from "./App.module.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [temp, setTemp] = useState([]);
+
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=50")
       .then((res) => res.json())
-      .then((data) => setUsers(data.results));
+      .then((data) => {
+        setUsers(data.results);
+        setTemp(data.results);
+      });
 
     return () => {};
   }, []);
+  function filterUser(textInput) {
+    const filteredUsers = temp.filter((user) => {
+      const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+      const phoneNumber = user.phone;
+      return (
+        fullName.toLowerCase().includes(textInput.toLowerCase()) ||
+        phoneNumber.includes(textInput)
+      );
+    });
+    setUsers(filteredUsers);
+  }
 
-  console.log(users);
   return (
     <div className={style.mainContainer}>
-      <Header />
+      <Header filterUser={filterUser} />
       <div className={style.cardContainer}>
         {users.map((user) => (
           <Contact
--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,10 +1,13 @@
 import style from "./index.module.css";
 
-function Header() {
+function Header(props) {
   return (
     <header className={style.header}>
       <h1>Contact List</h1>
       <input
+        onChange={(e) => {
+          props.filterUser(e.target.value);
+        }}
         type="text"
         placeholder="Search Name or Phone"
         style={{ padding: "25px", borderRadius: "10px" }}
